9026ea2b1b180b4957cef03c8f839490bc04e3be
Nick Cleary <nickcleary@Nicks-Air.local.nicleary.com>
2023-10-10 21:43:47 -0400

    Updates to add some stuff

--- shop_gen/gen/models.py
+++ shop_gen/gen/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+from django.db.models import Model, CharField, IntegerField, ForeignKey, BooleanField, CASCADE
 
-# Create your models here.
+
+class System(Model):
+    abbreviation: CharField(max_length=8, null=False)
+    full_name: CharField(max_length=64, null=False)
+
+
+class Item(Model):
+    name: CharField(max_length=128, null=False)
+    description: CharField(max_length=4096, null=True)
+    system: ForeignKey(System, on_delete=CASCADE, null=False)
+    price: IntegerField()
