8f58f5b7398d13cec3e269e9ba0cd1e93d0b35bd
Nick Cleary <nicholascleary01@gmail.com>
2024-02-14 22:20:21 -0500

    Working on things

--- shop_gen/settings.py
+++ shop_gen/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     "drf_spectacular",
     "django_extensions",
     "django_celery_results",
+    'django_tables2'
 ]
 
 MIDDLEWARE = [
@@ -57,6 +58,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.request"
             ],
         },
     },
